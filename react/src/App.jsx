23d248353627993e560308916cd295034c1590a5
Sharanpreet Kaur <001sharan001@gmail.com>
2026-09-13 14:24:25 +0530

    Add React project

---
+++ react/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Components/Navbar";
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <h1>App component</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ react/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import {Link} from "react-router";
+function Navbar() {
+    return (
+        <nav>
+            <link to = "/">Home</link>
+            <link to = "/about">About</link>
+            <link to = "/products">Products</link>
+            <link to = "/login">Login</link>
+        </nav>
+
+        
+    )
+}
